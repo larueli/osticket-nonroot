062d6959d0cee6fd90a5e723528c785373d7d8ad
Ivann LARUELLE <ivann+git@laruelle.me>
2021-04-27 13:45:21 +0200

    tmp fix for issue between cas and osticket

--- check_install.sh
+++ check_install.sh
@@ -3,3 +3,8 @@ if [ -n "${IS_INSTALLED}" ]
 then
     rm -rf /var/www/html/setup
 fi
+
+if [ -n "${NEEDS_PATCH_REGISTER}" ]
+then
+    sed -i 's/!$cfg || !$cfg->isClientRegistrationEnabled()/!$cfg/g' /var/www/html/include/class.auth.php
+fi
